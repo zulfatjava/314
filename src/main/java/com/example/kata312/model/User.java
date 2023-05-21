bd0d828d5803a7feae3ba3dab437462903501f1a
Zulfat <zulfatjava@gmail.com>
2023-05-21 08:27:32 +0300

    add

--- src/main/java/com/example/kata312/model/User.java
+++ src/main/java/com/example/kata312/model/User.java
@@ -14,7 +14,7 @@ import java.util.Objects;
 import java.util.Set;
 
 @Entity
-@Table(name = "Users")
+@Table(name = "users")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
